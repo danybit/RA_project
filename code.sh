7499090e3e25f07464233e5553dfa2b819686058
dany <danymukesha@gmail.com>
2021-02-19 17:05:12 +0000

    update 32

--- code.sh
+++ code.sh
@@ -13,15 +13,15 @@ ls *txt > file
 cat file | while read storage;
 do
 	
-	#mkdir -p ${storage::-4}						#making the celltype folder						#entering the celltype folder
+	mkdir -p ${storage::-4}						#making the celltypefolder
+	cd ${storage::-4}						 			#entering the celltype folder
 	cat $PWD"/"${datastorage[$j]}"/"${storage::-4}".txt" | while read line; 
 	do 
 		#echo $line
 		#Selecting only Bed files
 		if [[ ${line:(-3)} == "Bed" ]];then  
-			echo $line;
-			#wget $line;
-			#echo *Bed $storage; 
+			#echo $line;
+			echo wget $line; 
 		fi
 	done
 	cd ..
