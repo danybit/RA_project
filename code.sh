b65269a1c466d57a3ebf40fc8e132a6d56979d07
dany <danymukesha@gmail.com>
2021-02-19 17:16:48 +0000

    update 33

--- code.sh
+++ code.sh
@@ -1,33 +1,29 @@
 datastorage=("Encode" "Blueprint" "NIH_Roadmap")
-
+				
 lenStorage=${#datastorage[@]}
  	
 for ((j=0; j<$lenStorage; j++));
 do
+	cd ${datastorage[$j]}
+	echo ${datastorage[$j]}
+	ls *txt > file
 
-cd ${datastorage[$j]}
-
-echo ${datastorage[$j]}
-ls *txt > file
-
-cat file | while read storage;
-do
+	cat file | while read storage;
+	do
+		pwd=$PWD"/"
+		mkdir -p ${storage::-4}			#making the celltypefolder
+		cd ${storage::-4}			#entering the celltype folder
+		cat $pwd${storage::-4}".txt" | while read line;
+		do 
+			#Selecting only Bed files
+			if [[ ${line:(-3)} == "Bed" ]];then  
+				wget $line; 		#Downloading
+			fi
+		done
+		cd ..
 	
-	mkdir -p ${storage::-4}						#making the celltypefolder
-	cd ${storage::-4}						 			#entering the celltype folder
-	cat $PWD"/"${datastorage[$j]}"/"${storage::-4}".txt" | while read line; 
-	do 
-		#echo $line
-		#Selecting only Bed files
-		if [[ ${line:(-3)} == "Bed" ]];then  
-			#echo $line;
-			echo wget $line; 
-		fi
 	done
 	cd ..
-	
-done
-cd ..
 done
 
 
