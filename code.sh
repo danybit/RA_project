9fc3bc9cc4f8ddccfd9f72237338f94bde3e0275
danybit <45208254+danybit@users.noreply.github.com>
2021-02-23 10:03:42 +0000

    Update code.sh

--- code.sh
+++ code.sh
@@ -11,16 +11,32 @@ do
 	cat file | while read storage;
 	do
 		pwd=$PWD"/"
-		mkdir -p ${storage::-4}			#making the celltypefolder
+		mkdir -p ${storage::-4}			#making the celltypefolder	
 		cd ${storage::-4}			#entering the celltype folder
 		cat $pwd${storage::-4}".txt" | while read line;
 		do 
+			
+			
 			#Selecting only Bed files
-			if [[ ${line:(-3)} == "Bed" ]];then  
-				wget -nc $line; 			#Downloading
+			if [[ ${line:(-3)} == "Bed" ]];then 
 				bbedfile=${line##*/}
-				bigBedToBed $bbedfile stdout | gzip -c > ${bbedfile::(-7)}.bed.gz; #transforming into bed
-				rm $bbedfile; 				#removing bigbed
+				bb_file=${bbedfile::(-7)}.bed.gz
+				if [[ -f $bb_file ]]
+				then
+					echo "$line exists on your filesystem."
+					#transforming into bed
+					if [[ ${bbedfile:(-3) == "Bed"} ]]
+					then
+						bigBedToBed $bbedfile stdout | gzip -c > ${bbedfile::(-7)}.bed.gz; 
+						rm $bbedfile; 	#removing bigbed
+					fi
+				else
+					echo "$line doesn't exist on your filesystem. NOW DOWNLOAD!!" 
+					wget -nc $line; 			#Downloading
+					#transforming into bed
+					bigBedToBed $bbedfile stdout | gzip -c > ${bbedfile::(-7)}.bed.gz; 
+					rm $bbedfile; 				#removing bigbed
+				fi
 			fi
 		done
 		cd ..
